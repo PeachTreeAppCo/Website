2299fba111cdea2061f17d42ca47b4e5ae6fc86c
JoeScho <joeschofield@live.co.uk>
2017-02-02 20:41:57 +0000

    Initial website upload

---
+++ PeachTreeWebsite/PeachTreeWebsite/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace PeachTreeWebsite.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ PeachTreeWebsite/PeachTreeWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PeachTreeWebsite.Startup))]
+namespace PeachTreeWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
