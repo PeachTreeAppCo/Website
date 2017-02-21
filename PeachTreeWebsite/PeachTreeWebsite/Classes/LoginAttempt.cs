86ac3b888efd86129451b5be7a13acc2ec60c5cd
Joe Schofield <joeschofield@live.co.uk>
2017-02-21 09:54:29 +0000

    Begin loginAttempt code

---
+++ PeachTreeWebsite/PeachTreeWebsite/Classes/LoginAttempt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PeachTreeWebsite.Classes
+{
+    public class LoginAttempt
+    {
+        private int attemptNo;
+        // create attributes & constructors, then add table to failed login page and add button to home page for admin to use
+    }
+}
